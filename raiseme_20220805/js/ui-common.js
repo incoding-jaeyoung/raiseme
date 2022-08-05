c1a7891f55a177f2e5a5e5ee50c68f557084d3b1
jojaeyoung <contact@incoding.co.kr>
2022-08-05 19:00:16 +0900

    no message

--- raiseme_20220805/js/ui-common.js
+++ raiseme_20220805/js/ui-common.js
@@ -225,7 +225,7 @@ function commonTween() {
 						start: "0% 100%", // 앞 : 객체 , 뒤 : 페이지 전체
 						end: "0% 50%", // 앞 : 객체 , 뒤 : 페이지 전체
 						scrub: true, //스크롤에 반응 (없으면 자동재생)
-						//markers: true,
+						markers: true,
 						toggleActions: "play pause pause reverse",
 					},
 				});
